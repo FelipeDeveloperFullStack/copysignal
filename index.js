270e91b6805e752f4600cc54a09efe474d32cade
Felipe Miguel Dos Santos <felipe.msantos@gruposaga.com.br>
2020-01-10 15:05:11 -0300

    Commit

--- index.js
+++ index.js
@@ -8,24 +8,70 @@ let copy = async () => {
 
     try {
 
-        await driver.get('https://br.investing.com/indices/ibovespa-futures-technical')
+        //await driver.get('https://br.investing.com/indices/ibovespa-futures-technical')
 
-        await driver.getTitle().then((title) => {
+        /*await driver.getTitle().then((title) => {
             console.log('The title is: ' + title)
         })
+*/
+        //console.log('\n')
 
-        console.log('\n')
+        await driver.get('https://br.investing.com/indices/ibovespa-futures-technical');
+
+        while (true) {
+            await driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[1]/a')).click()
+            await driver.sleep(3000)
+            let elements1min = await driver.findElements({ css: '#techStudiesInnerWrap > div.summary > span' });
+            for (let element of elements1min) {
+                text = await element.getText();
+                console.log('1 MIN: ' + text);
+            }
+            console.log('\n')
+            
+
+            await driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[2]/a')).click()
+            await driver.sleep(3000)
+            let elements5min = await driver.findElements({ css: '#techStudiesInnerWrap > div.summary > span' });
+            for (let hora of elements5min) {
+                text = await hora.getText();
+                console.log('5 MIN: ' + text);
+            }
+            console.log('\n')
+
+            await driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[3]/a')).click()
+            await driver.sleep(3000)
+            let elements15min = await driver.findElements({ css: '#techStudiesInnerWrap > div.summary > span' });
+            for (let hora of elements15min) {
+                text = await hora.getText();
+                console.log('15 MIN: ' + text);
+            }
+            console.log('\n')
+
+            await driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[4]/a')).click()
+            await driver.sleep(3000)
+            let elements30min = await driver.findElements({ css: '#techStudiesInnerWrap > div.summary > span' });
+            for (let hora of elements30min) {
+                text = await hora.getText();
+                console.log('30 MIN: ' + text);
+            }
+            console.log('\n')
+        }
+
+
+        //return driver.quit();
 
         //1 min
         //await driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[1]')).click()
 
-        setTimeout(() => {
-            console.log('> Signal em 1 Hora')
-            driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[5]/a')).click()
-            let elementText1min = driver.findElement(By.xpath('/html/body/div[5]/section/div[10]/div[1]/div[1]/span'))
-            driver.wait(until.elementIsVisible(elementText1min), 4000)
-            Promise.resolve(elementText1min.getText()).then(text => { console.log('RESUMO: ' + text) })
-        }, 5000)
+        /*console.log('> Signal em 1 Hora')
+        driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[5]/a')).click()
+        let elementText1min = driver.findElements(By.className('summary'))
+        elementText1min.then(el =>{
+            console.log(el)
+        })*/
+
+        //driver.wait(until.elementIsVisible(elementText1min), 4000)
+        //Promise.resolve(elementText1min.getText()).then(text => { console.log('RESUMO: ' + text) })
 
         //let text1min = await driver.findElement(By.xpath('/html/body/div[5]/section/div[10]/div[1]/div[1]/span')).getText()
         //console.log(text1min)
