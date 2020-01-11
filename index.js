10833fb7479d8e918c9635552a06c6f385299560
Felipe Miguel <felipe.miguel.santos@hotmail.com>
2020-01-10 23:32:05 -0300

    Implementacao de sinal

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Builder, By, Key, logging, until, WebElement } = require('selenium-webdriver');
+const axios = require('axios')
 
 //logging.installConsoleHandler();
 //logging.getLogger('webdriver.http').setLevel(logging.Level.ALL);
@@ -8,33 +9,50 @@ let copy = async () => {
 
     try {
 
-        //await driver.get('https://br.investing.com/indices/ibovespa-futures-technical')
-
-        /*await driver.getTitle().then((title) => {
-            console.log('The title is: ' + title)
-        })
-*/
-        //console.log('\n')
-
         await driver.get('https://br.investing.com/indices/ibovespa-futures-technical');
-        await driver.sleep(60000)
+        await driver.sleep(10000)
 
         while (true) {
+
+            //COMPRA
+            let sinalCompra1min = false
+            let sinalCompra5min = false
+            let sinalCompra15min = false
+            let sinalCompra30min = false
+
+            //VENDA
+            let sinalVenda1min = false
+            let sinalVenda5min = false
+            let sinalVenda15min = false
+            let sinalVenda30min = false
+
+
             await driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[1]/a')).click()
             await driver.sleep(3000)
             let elements1min = await driver.findElements({ css: '#techStudiesInnerWrap > div.summary > span' });
             for (let element of elements1min) {
                 text = await element.getText();
+                if (text === 'VENDA FORTE') {
+                    sinalVenda1min = true
+                }
+                if (text === 'COMPRA FORTE') {
+                    sinalCompra1min = true
+                }
                 console.log('1 MIN: ' + text);
             }
             console.log('\n')
-            
 
             await driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[2]/a')).click()
             await driver.sleep(3000)
             let elements5min = await driver.findElements({ css: '#techStudiesInnerWrap > div.summary > span' });
             for (let hora of elements5min) {
                 text = await hora.getText();
+                if (text === 'VENDA FORTE') {
+                    sinalVenda5min = true
+                }
+                if (text === 'COMPRA FORTE') {
+                    sinalCompra5min = true
+                }
                 console.log('5 MIN: ' + text);
             }
             console.log('\n')
@@ -44,6 +62,12 @@ let copy = async () => {
             let elements15min = await driver.findElements({ css: '#techStudiesInnerWrap > div.summary > span' });
             for (let hora of elements15min) {
                 text = await hora.getText();
+                if (text === 'VENDA FORTE') {
+                    sinalVenda15min = true
+                }
+                if (text === 'COMPRA FORTE') {
+                    sinalCompra15min = true
+                }
                 console.log('15 MIN: ' + text);
             }
             console.log('\n')
@@ -53,32 +77,34 @@ let copy = async () => {
             let elements30min = await driver.findElements({ css: '#techStudiesInnerWrap > div.summary > span' });
             for (let hora of elements30min) {
                 text = await hora.getText();
+                if (text === 'VENDA FORTE') {
+                    sinalVenda30min = true
+                }
+                if (text === 'COMPRA FORTE') {
+                    sinalCompra30min = true
+                }
                 console.log('30 MIN: ' + text);
             }
             console.log('\n')
+
+            await driver.sleep(2000)
+            if (sinalCompra1min === true && sinalCompra5min === true
+                && sinalCompra15min === true && sinalCompra30min === true) {
+                axios.put('https://copysignal-4e7c8.firebaseio.com/sinais.json', { sinal: 'compra' }).then(resp => {
+                    console.log('Sinal de compra enviado!')
+                })
+            } else if (sinalVenda1min === true && sinalVenda5min === true
+                && sinalVenda15min === true && sinalVenda30min === true) {
+                axios.put('https://copysignal-4e7c8.firebaseio.com/sinais.json', { sinal: 'venda' }).then(resp => {
+                    console.log('Sinal de venda enviado!')
+                })
+            } else {
+                axios.put('https://copysignal-4e7c8.firebaseio.com/sinais.json', { sinal: 'neutro' }).then(resp => {
+                    console.log('Nenhum sinal! Operação neutra!')
+                    console.log('\n')
+                })
+            }
         }
-
-
-        //return driver.quit();
-
-        //1 min
-        //await driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[1]')).click()
-
-        /*console.log('> Signal em 1 Hora')
-        driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[5]/a')).click()
-        let elementText1min = driver.findElements(By.className('summary'))
-        elementText1min.then(el =>{
-            console.log(el)
-        })*/
-
-        //driver.wait(until.elementIsVisible(elementText1min), 4000)
-        //Promise.resolve(elementText1min.getText()).then(text => { console.log('RESUMO: ' + text) })
-
-        //let text1min = await driver.findElement(By.xpath('/html/body/div[5]/section/div[10]/div[1]/div[1]/span')).getText()
-        //console.log(text1min)
-        //await driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[2]')).click()
-        //await driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[3]')).click()
-        //await driver.findElement(By.xpath('/html/body/div[5]/section/div[8]/ul/li[4]')).click()
 
     } catch (error) {
         handleError(error, driver)
